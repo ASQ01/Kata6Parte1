d39256ab48e31951fd356e4305fc28baaf928498
ASQ01 <alvarosq2001@gmail.com>
2021-11-29 16:38:25 +0000

    Kata6P1 v2

--- src/kata6parte1/main/Main.java
+++ src/kata6parte1/main/Main.java
@@ -8,16 +8,33 @@ import kata6parte1.toys.*;
 public class Main {
     public static void main(String[] args){
         SerialNumberGenerator serial = new SerialNumberGenerator();
+        ArrayList<Car> cars = new ArrayList<>();
         
         Scanner input = new Scanner(System.in);
         String choice = "";
         while (!choice.toUpperCase().equals("EXIT")){
             System.out.print("Toy to create: ");
             choice = input.nextLine();
-            if (!choice.toUpperCase().equals("EXIT")){
-                Car car = new Car(serial.next());
-                car.pack();
-                car.label();
+            switch(choice.toUpperCase()){
+                case "CAR":
+                    Car car = new Car(serial.next());
+                    car.pack();
+                    car.label();
+                    break;
+                    
+                case "HELICOPTER":
+                    Helicopter helicopter = new Helicopter(serial.next());
+                    helicopter.pack();
+                    helicopter.label();
+                    break;
+                    
+                case "EXIT":
+                    System.out.println("Goodbye");
+                    break;
+                    
+                default:
+                    System.out.println("Not recognizeable toy");
+                    break;
             }
         }
         System.exit(0);
